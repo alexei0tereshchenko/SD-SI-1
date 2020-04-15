d4c12d819ec14669d8443b391cf8416037be6310
lightcakess <lightcakess@gmail.com>
2020-04-16 00:17:04 +0300

    Added OtsController, add attributes to DTO, refactore

---
+++ demo/src/main/java/com/example/demo/dto/ots/CreateOtsDto.java
@@ -0,0 +1,15 @@
+package com.example.demo.dto.ots;
+
+import com.example.demo.model.ots.OtsStatus;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class CreateOtsDto {
+    private Long parentId;
+    private BigDecimal amount;
+    private OtsStatus otsStatus;
+    private String terminationReason;
+    private boolean instalments;
+}
---
+++ demo/src/main/java/com/example/demo/dto/ots/UpdateOtsDto.java
@@ -0,0 +1,18 @@
+package com.example.demo.dto.ots;
+
+import com.example.demo.model.ots.OtsStatus;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+public class UpdateOtsDto {
+    private String description;
+    private Date createdWhen;
+    private BigDecimal amount;
+    private Long parentId;
+    private OtsStatus otsStatus;
+    private String terminationReason;
+    private boolean instalments;
+}
