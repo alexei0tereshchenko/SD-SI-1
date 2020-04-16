44325d9aa01e833819500bfdce6bb79ae3bc458a
Nikita Tev <tev.forwork@gmail.com>
2020-04-16 23:00:23 +0300

    added data formatter to EavBaseConverter

--- demo/src/main/java/com/example/demo/eav/converter/EavBaseConverter.java
+++ demo/src/main/java/com/example/demo/eav/converter/EavBaseConverter.java
@@ -6,10 +6,11 @@ import com.example.demo.eav.model.object.Object;
 import com.example.demo.eav.model.object.Param;
 import com.example.demo.eav.model.object.Reference;
 import com.example.demo.model.Base;
-import com.example.demo.model.Status;
+import com.example.demo.model.customer.Status;
 import com.example.demo.model.payment.*;
 import com.example.demo.repository.ObjectRepository;
 import com.example.demo.repository.ObjectTypeRepository;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -30,7 +31,8 @@ public abstract class EavBaseConverter {
     @Autowired
     private ObjectRepository objectRepository;
 
-    private SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    @Getter
+    private static SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     public Object convertToEav(Base source) {
         Object target = new Object();
@@ -91,65 +93,5 @@ public abstract class EavBaseConverter {
         return target;
     }
 
-    public <T extends Base> T convertFromEav(Object source, T target){
-        target.setName(source.getName());
-        target.setObjectId(source.getObjectId());
-        target.setObjectTypeId(source.getObjectType().getObjectTypeId());
-        if(source.getParentObject() != null) {
-            target.setParentId(source.getParentObject().getObjectId());
-        }
-
-        List<Param> params = source.getParams();
-
-        for (Param param : params){
-            Long attributeId = param.getAttribute().getAttributeId();
-            String value = param.getValue();
-
-            for(Field declaredField : target.getClass().getDeclaredFields()){
-                if(declaredField.getDeclaredAnnotation(Attribute.class) != null
-                        && attributeId.equals(Long.valueOf(declaredField.getDeclaredAnnotation(Attribute.class).attrId()))){
-                    declaredField.setAccessible(true);
-                    try {
-                        if(declaredField.getType() == String.class) {
-                            declaredField.set(target, value);
-                        }
-                        else if(declaredField.getType() == Date.class){
-                            declaredField.set(target, dateFormatter.parse(value));
-                        }
-                        else if(declaredField.getType() == BigDecimal.class) {
-                            declaredField.set(target, new BigDecimal(value));
-                        }
-                        else if(declaredField.getType() == PaymentStatus.class) {
-                            declaredField.set(target, PaymentStatus.valueOf(value));
-                        }
-                        else if(declaredField.getType() == PaymentMethod.class) {
-                            declaredField.set(target, PaymentMethod.valueOf(value));
-                        }
-                        else if(declaredField.getType() == Status.class) {
-                            declaredField.set(target, Status.valueOf(value));
-                        }
-                    } catch (IllegalAccessException | ParseException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            }
-
-            List<Reference> references = source.getReferences();
-
-            // add code for reference parsing
-        }
-
-        return target;
-    }
-
-    // returns declared attribute_id for the field by its name
-    public <T extends Base> Long getAttributeIdForField(T target, String fieldName) throws NoSuchFieldException {
-        if(target.getClass().getDeclaredField(fieldName).getDeclaredAnnotation(Attribute.class) != null){
-            return Long.valueOf(target.getClass().getDeclaredField(fieldName).getDeclaredAnnotation(Attribute.class).attrId());
-        }
-        else{
-            return -1L;
-        }
-    }
-
+    public abstract Base convertFromEav (Object source);
 }
