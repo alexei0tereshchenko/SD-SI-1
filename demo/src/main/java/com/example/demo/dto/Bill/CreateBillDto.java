a58bcf5605b3ed7bcb18daab8d6d8208b3900504
Pirofany <danikthebest@tut.by>
2020-04-16 14:26:57 +0300

    add service,integration controller, updatebilldto

--- demo/src/main/java/com/example/demo/dto/Bill/CreateBillDto.java
+++ demo/src/main/java/com/example/demo/dto/Bill/CreateBillDto.java
@@ -1,12 +1,11 @@
 package com.example.demo.dto.Bill;
 
 import com.example.demo.model.Bill.BillStatus;
-import com.example.demo.model.Bill.BillStyle;
 import lombok.Data;
 
 @Data
 public class CreateBillDto {
-    public BillStatus BillStatus;
-    public enum BillStyle;
-    public long BillNumber;
+    public BillStatus billStatus;
+    public enum billStyle;
+    public long billNumber;
 }
