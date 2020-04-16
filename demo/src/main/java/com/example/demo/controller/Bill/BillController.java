38e21ca17f2c917aef4e00f726d4d241876aa0dc
Pirofany <danikthebest@tut.by>
2020-04-16 14:54:13 +0300

    fix

--- demo/src/main/java/com/example/demo/controller/Bill/BillController.java
+++ demo/src/main/java/com/example/demo/controller/Bill/BillController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/bill")
 public class BillController {
     @Autowired
     BillService billService;
