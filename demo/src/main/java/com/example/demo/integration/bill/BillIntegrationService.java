506ab05a15bf72104d014bd683ec9efe37918ecf
Pirofany <danikthebest@tut.by>
2020-05-07 16:42:08 +0300

    fix

--- demo/src/main/java/com/example/demo/integration/bill/BillIntegrationService.java
+++ demo/src/main/java/com/example/demo/integration/bill/BillIntegrationService.java
@@ -27,7 +27,6 @@ public class BillIntegrationService {
     @Autowired
     private AttributeRepository attributeRepository;
 
-    @Autowired
     private EavBaseConverter eavBaseConverter;
 
     @Autowired
